50f55a4a0cbb896ea05aacbcac08ac59e1907261
tashisrael <tashisrael04@gmail.com>
2024-11-22 12:37:24 +0300

    changes

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', () => {
+        const nav = document.querySelector('nav');
+        nav.classList.remove('open');
+    });
+});
