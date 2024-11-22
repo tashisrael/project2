9554ac1e1e3a795671dee27a9b9d88473620c749
tashisrael <tashisrael04@gmail.com>
2024-11-22 12:53:25 +0300

    changes

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
+    const menuButton = document.getElementById('menuButton');
+    if (nav.classList.contains('open')) {
+        menuButton.innerHTML = '<i class="bx bx-x"></i>'; // Change to close icon
+    } else {
+        menuButton.innerHTML = '<i class="bx bx-menu"></i>'; // Change back to menu icon
+    }
 }
 
 document.querySelectorAll('nav ul li a').forEach(link => {
     link.addEventListener('click', () => {
         const nav = document.querySelector('nav');
         nav.classList.remove('open');
+        const menuButton = document.getElementById('menuButton');
+        menuButton.innerHTML = '<i class="bx bx-menu"></i>'; // Change back to menu icon
     });
 });
